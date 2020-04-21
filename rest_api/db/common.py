72ac1875c9258efd1e04d5ed1a47b3069711594c
Abdul Fatir <a.fatir@yahoo.com>
2020-04-21 22:49:01 +0100

    Fixed ImportError: cannot import name 'ReqlNonExistenceError'

--- rest_api/db/common.py
+++ rest_api/db/common.py
@@ -14,7 +14,7 @@
 # ------------------------------------------------------------------------------
 
 from rethinkdb import RethinkDB
-from rethinkdb import ReqlNonExistenceError
+from rethinkdb.errors import ReqlNonExistenceError
 
 from api.errors import ApiInternalError
 
